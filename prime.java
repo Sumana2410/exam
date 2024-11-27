936cc7280e24c56a7e133379c2c51b71427c55f2
Sumana2410 <sumanasreepiratla2410@gmail.com>
2024-11-27 21:02:24 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,18 +1,5 @@
-package exam;
 class Prime {
   public static void main(String[] args) {
-      int n = 29;
-      boolean isPrime = true;
-      for (int i = 2; i <= n / 2; ++i) {
-          if (n % i == 0) {
-              isPrime = false;
-              break;
-          }
-      }
-      if (isPrime) {
-          System.out.println(n + " is a prime number.");
-      } else {
-          System.out.println(n + " is not a prime number.");
-      }
+      System.out.println("hello");
   }
 }
